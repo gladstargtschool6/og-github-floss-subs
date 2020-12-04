14ab4125ea7884abd2996ce570ffbd6bee0aa557
osha7 <ogroetz@gmail.com>
2020-12-04 13:47:38 -0700

    debugger during build

--- components/Pricing.js
+++ components/Pricing.js
@@ -72,12 +72,11 @@ export default function Pricing({ products }) {
         </div>
         <div className="mt-12 space-y-4 sm:mt-16 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-6 lg:max-w-4xl lg:mx-auto xl:max-w-none xl:mx-0 xl:grid-cols-4">
           {products.map((product) => {
-            // debugger;
-            const price = product.prices[0]
-            // const price = product.prices.find(
+            debugger;
+            const price = product.prices.find(
               
-            //   (price) => price.interval === billingInterval
-            // );
+              (price) => price.interval === billingInterval
+            );
             const priceString = new Intl.NumberFormat('en-US', {
               style: 'currency',
               currency: price.currency,
