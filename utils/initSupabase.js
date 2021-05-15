460d23d515d0140a18c6da68762942e122c4c5e6
Joseph Moshood <gladstargtschool6@gmail.com>
2021-05-15 09:40:52 +0100

    Update initSupabase.js

--- utils/initSupabase.js
+++ utils/initSupabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.imdvvfcbtrfewysxgrnr.supabase.co,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
 
